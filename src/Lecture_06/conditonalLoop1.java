83dfbf39b82081bce98750cd855b4de2f9280df2
mukund-ahire <mukundahire.work@gmail.com>
2025-05-09 09:14:36 +0530

    Lecture 06 added and if else condition code updated

---
+++ src/Lecture_06/conditonalLoop1.java
@@ -0,0 +1,27 @@
+package Lecture_06;
+
+import java.util.Scanner;
+
+public class conditonalLoop1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        /*
+            Syntax of if statements:
+            if ( boolean expression True or False){
+                // body
+            }else {
+                // do this
+            }
+         */
+
+        System.out.print("Enter the Salary to check the bonus : ");
+        int salary = input.nextInt();
+        if (salary > 10000){
+            salary = salary + 2000;
+        } else {
+            salary = salary + 1000;
+        }
+
+        System.out.print("Your Salary after bonus is : "+ salary);
+    }
+}
